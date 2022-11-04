6bf5af1b6e82048101ac8c0032c31f85f02d8e67
SeungA <seungashin9275@gmail.com>
2022-11-04 12:57:42 +0900

    Create 03_write_df_to_sh.py

---
+++ 03_write_df_to_sh.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+from sa_package.my_gspread import write_df_to_sh
+
+SCOPE = ["https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"]
+CREDS = ServiceAccountCredentials.from_json_keyfile_name("C:/Users/SANDBOX/sandbox_workspace/data_management/dm_workshop/coding_day/key.json", SCOPE)
+SH_ID = "1l1LhuVXF5WmRX2V-LnJArAYN3Gc_4mMSWKcCO---Sxg"
+
+
+test_df = pd.DataFrame(
+    data={
+        "col1":[1, 2, 3],
+        "col2":[4, 5, 6]
+    }
+)
+
+write_df_to_sh(
+    df=test_df,
+    sh_id=SH_ID,
+    worksheet_name="승아",
+    include_header=False,
+    start_cell="A2",
+    creds=CREDS
+)
+
+write_df_to_sh(
+    df=test_df,
+    sh_id=SH_ID,
+    worksheet_name="승아",
+    include_header=True,
+    start_cell="D1",
+    creds=CREDS
+)
